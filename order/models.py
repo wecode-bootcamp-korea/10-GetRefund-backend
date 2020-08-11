e85b2378e2705e22f08437ec12ac6814114f830c
Sol-Ahn <sorry0194@gmail.com>
2020-08-11 21:04:11 +0900

    [환불원정단 안솔] Add: order/models.py 추가

---
+++ order/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from user.models    import User
+from product.models import (
+    Product,
+    Color,
+    Image,
+    ImageCategory,
+)
+
+class Order(models.Model):
+    order_status = models.ForeignKey("OrderStatus", on_delete=models.CASCADE, default="")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default="")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, default="")
+    quantity = models.IntegerField(default=0)
+    ordered_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "orders"
+        
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=50, default="")
+
+    class Meta:
+        db_table = "order_statuses"
+
