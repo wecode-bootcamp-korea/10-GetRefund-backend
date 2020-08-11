8eedff060015d034aa33dd8d60f051320380d4a5
soomialexhwang <ifyouseeksoomi@gmail.com>
2020-08-11 15:39:20 +0900

    [환불원정단 황수미] Fix: logindecorator 리뷰 반영 (Keyerror) 및 views.py 재삭제

---
+++ user/utils.py
@@ -0,0 +1,24 @@
+import jwt, json
+
+from django.http    import JsonResponse
+from my_settings    import SECRET_KEY, ALGORITHM
+from .models        import User
+
+def login_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers.get('Authorization', None)
+            data         = jwt.decode(access_token, SECRET_KEY['secret'], ALGORITHM['algorithm'])
+            user         = User.objects.get(id=data['user_id'])
+            request.user = user
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse ({'message': 'INVALID_TOKEN'}, status=401)
+        except User.DoesNotExist:
+            return JsonResponse ({'message': 'INVALID_USER'}, status=400)
+        except KeyError:
+            return JsonResponse ({'message': 'KEY_ERROR'}, status=400)
+        return func(self, request, *args, **kwargs)
+    return wrapper
+
+
