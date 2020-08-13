e1cb813538f19548fea700914a9672f8b4701136
Sol-Ahn <sorry0194@gmail.com>
2020-08-13 23:20:16 +0900

    [환불원정단 안솔] Modify: product/urls.py 수정

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import DetailInfoView
+from .views import ProductDetailView, PairWithView, DetailInfoView
 
 urlpatterns = [
-#    path('/detail/<int:product_id>', ProductDetailView.as_view()),
-#    path('/detail/<int:product_id>/pairwith', PairWithView.as_view()),
+    path('/detail/<int:product_id>', ProductDetailView.as_view()),
+    path('/detail/<int:product_id>/pairwith', PairWithView.as_view()),
     path('/detail/<int:product_id>/detailinfo', DetailInfoView.as_view())
 ]
 
